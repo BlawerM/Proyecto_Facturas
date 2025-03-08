0f95b2cbf8e689fb81a7e202408bff8d239a9855
Blawer <blawermoncada@gmail.com>
2025-03-08 07:38:50 -0500

    Iniciar proyecto

--- Modelo/Producto.php
+++ Modelo/Producto.php
@@ -1,3 +1,29 @@
 <?php
+class Producto {
+    private $propiedades = [];
+    private $propiedades = [];
+    private $propiedades = [];
+    private $propiedades = [];
 
+    function __construct($Codigo, $Nombre, $Stock, $ValorUnitario) {
+        $this->propiedades = $params;
+    }
+
+    public function __set($nombre, $valor) {
+        $this->propiedades[$nombre] = $valor;
+    }
+
+    public function __get($nombre) {
+        if (array_key_exists($nombre, $this->propiedades)) {
+            return $this->propiedades[$nombre];
+        }
+        trigger_error("Propiedad no definida: " . $nombre, E_USER_NOTICE);
+        return null;
+    }
+
+    // Método para obtener todas las propiedades
+    public function obtenerPropiedades() {
+        return $this->propiedades;
+    }
+}
 ?>
