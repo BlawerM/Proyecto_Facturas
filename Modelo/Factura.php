c558b7a81fb54df70873cf61a859acc061120579
Blawer <blawermoncada@gmail.com>
2025-03-15 10:57:45 -0500

    Factura hecha

---
+++ Modelo/Factura.php
@@ -0,0 +1,66 @@
+<?php
+class Factura {
+    private $Cod_Factura;
+    private $Fecha_Factura;
+    private $Nro_Factura;
+    private $Total_Factura;
+    private $Cod_PersonaFK;
+
+    function __construct($Cod_Factura, $Fecha_Factura, $Nro_Factura, $Total_Factura, $Cod_PersonaFK) {
+        $this->$Cod_Factura = $Cod_Factura;
+        $this->$Fecha_Factura = $Fecha_Factura;
+        $this->$Nro_Factura = $Nro_Factura;
+        $this->$Total_Factura = $Total_Factura; 
+        $this->$Cod_PersonaFK = $Cod_PersonaFK;    
+    }
+    #region SetGetCodigo
+    function setCod_Factura ($Cod_Factura){
+        $this->$Cod_Factura;
+    }
+
+    function getCod_Factura(){
+        return $this->Cod_Factura;
+    }
+    #endregion
+
+    #region SetGetFecha
+    function setFecha_Factura ($Fecha_Factura){
+        $this->$Fecha_Factura;
+    }
+
+    function getFecha_Factura (){
+        return $this->Fecha_Factura;
+    }
+    #endregion
+
+    #region SetGetNroFactura
+    function setNro_Factura ($Nro_Factura){
+        $this->$Nro_Factura;
+    }
+
+    function getNro_Factura(){
+        return $this->Nro_Factura;
+    }
+    #endregion
+
+    #region SetGetTotal
+    function setTotal_Factura ($Total_Factura){
+        $this->$Total_Factura;
+    }
+
+    function getTotal_Factura (){
+        return $this->Total_Factura;
+    }
+    #endregion
+
+    #region SetGetPersonaFK
+    function setCod_PersonaFK ($Cod_PersonaFK){
+        $this->$Cod_PersonaFK;
+    }
+    
+    function getCod_PersonaFK (){
+        return $this->Cod_PersonaFK;
+    }
+    #endregion
+}
+?>
